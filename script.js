ba7a48316024485767719ca2c7926b8c4c30657e
Miruna Craciun <157888228+mimi-mc@users.noreply.github.com>
2024-02-25 12:07:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const btn = document.getElementById('btn');
 const colors = ['green', 'yellow', 'red'];
 let colorIndex = 0;
+btn.addEventListener('click', changeColor);
 
-btn.addEventListener('click', function changeColor() {
+function changeColor() {
     btn.style.backgroundColor = colors[colorIndex];
     colorIndex = (colorIndex + 1) % colors.length;
-});
+    resetTimer();
+}
+
+let timer = setInterval(changeColor, 10000);
+
+function resetTimer() {
+    clearInterval(timer);
+    timer = setInterval(changeColor, 10000);
+}
